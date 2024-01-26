0f38b549eef7f670ffdb643908e3d28652e66d61
YithSopheaktra8 <yithsopheaktra18@gmail.com>
2024-01-27 03:45:15 +0700

    working with booking multiple seats

--- src/Main.java
+++ src/Main.java
@@ -9,51 +9,164 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        System.out.print("> Config total rows in hall : ");
-//        String row = input.nextLine();
-//        System.out.print("> Config total rows in hall : ");
-//        String col = input.nextLine();
 //
-//        String initializeRowCol = validateInputNumber(row) && validateInputNumber(col) ? "Successfully created Hall" : "row and column can be number only";
+        Integer row = validateInputNumber("> Config total rows in hall : ","only number from 1-9",input);
+        Integer col = validateInputNumber("> Config total cols in hall : ","only number from 1-9",input);
 //
-//        Integer[][] hallA = new Integer[stringToInt(row)][stringToInt(col)];
-//        Integer[][] hallB = new Integer[stringToInt(row)][stringToInt(col)];
-//        Integer[][] hallC = new Integer[stringToInt(row)][stringToInt(col)];
-//        System.out.println(Arrays.deepToString(hallA));
+        String[][] morningHall = new String[row][col];
+        String[][] afternoonHall = new String[row][col];
+        String[][] nightHall = new String[row][col];
+        initHall(morningHall);
+        initHall(afternoonHall);
+        initHall(nightHall);
 
-        String ch = "";
-        do {
-            System.out.println("Choose option : ");
-            ch = input.nextLine();
-            if(!validateInputChar(ch)){
-                System.out.println("Wrong command");
-            }
-        }while (true);
+        displayOneHall(morningHall);
+        chooseSeat(morningHall);
+        displayOneHall(morningHall);
+
+
+//        Character ch;
+//        do {
+//            mainMenu();
+//            ch = validateInputChar("Choose option : ","wrong command",input);
+//            switch (ch){
+//                case 'a' -> System.out.println("booking");
+////                case 'b' -> booking();
+//                case 'c' -> showTimeMenu();
+//                case 'f' -> System.exit(0);
+//            }
+//
+//
+//        }while (!(ch.equals('f')));
 
 
         
     }
 
-    // convert from String to Integer
-    public static int stringToInt(String value){
-        return Integer.parseInt(value);
+    //  validate char
+    public static Character validateInputChar(String message, String error, Scanner input){
+        while (true){
+            System.out.print(message);
+            String choice = input.nextLine();
+            Pattern pattern = Pattern.compile("[a-zA-Z]+");
+            Matcher matcher = pattern.matcher(choice);
+            if(matcher.matches())
+                return Character.toLowerCase(choice.charAt(0));
+            else
+                System.out.println(error);
+        }
     }
 
-    //  validate input character
-    public static boolean validateInputChar(String input){
-        Pattern pattern = Pattern.compile("[a-zA-Z]+");
-        Matcher matcher = pattern.matcher(input);
-        return matcher.matches();
+//    validate number
+    public static Integer validateInputNumber(String message, String error, Scanner input){
+        while (true){
+            System.out.print(message);
+            String choice = input.nextLine();
+            Pattern pattern = Pattern.compile("[0-9]+");
+            Matcher matcher = pattern.matcher(choice);
+            if(matcher.matches()){
+                if(!(Integer.parseInt(choice) == 0))
+                    return Integer.parseInt(choice);
+                else
+                    System.out.println("Can not be ZERO");
+            }
+            else
+                System.out.println(error);
+        }
     }
 
-    public static String validateInputNumber(String input , String successMsg, String errorMsg){
-        Pattern pattern = Pattern.compile("[1-9]+");
-        Matcher matcher = pattern.matcher(input);
-        return matcher.matches() ? successMsg : errorMsg;
+//    Booking seat
+    public static void booking(String[][] morningHall, String[][] afternoonHall, String[][] nightHall){
+        Scanner input = new Scanner(System.in);
+
+//        do {
+//            showTimeMenu();
+//            String[][] hall = getHall(morningHall,afternoonHall,nightHall);
+//            displayHall(hall,'A');
+//
+//        }while ()
+
+
     }
+
+//  getOneHall
+    public static String[][] getHall(String[][] morningHall, String[][] afternoonHall, String[][] nightHall){
+        Scanner input = new Scanner(System.in);
+        Character choice = validateInputChar("> Please select show time (A | B | C): ","Please input A-B-C",input);
+        if(choice.equals('a')){
+            return morningHall;
+        } else if (choice.equals('b')) {
+            return afternoonHall;
+        }else
+            return nightHall;
+    }
+
+    //   Initialize hall
+    public static void initHall(String[][] hall){
+        for (int i = 0; i<hall.length; i++){
+            for (int j = 0; j<hall[i].length; j++){
+                hall[i][j] = "AV";
+            }
+        }
+    }
+
+//  display Hall
+    public static void displayOneHall(String[][] hall){
+        System.out.println("+".repeat(60));
+        for (int i = 0; i<hall.length; i++){
+            for (int j= 0; j<hall[i].length; j++){
+                char aphabet = (char) ('A' + i ) ;
+                System.out.print("|"+aphabet+"-"+(1+j)+"::"+hall[i][j]+"|\t");
+            }
+            System.out.println();
+        }
+        System.out.println("+".repeat(60));
+    }
+
+//   chooseSeat
+    public static void chooseSeat(String[][] hall){
+        Scanner input = new Scanner(System.in);
+        char c = 67;
+        int number = 2;
+        String[] stringArray = new String[0];
+        // Input strings dynamically
+        while (true) {
+            System.out.print("Enter a string (press Enter to finish): ");
+            String userInput = input.nextLine();
+
+            // Split the input string by commas
+            String[] substrings = userInput.split(",");
+
+            // Add the substrings to the array
+            String[] newArray = new String[stringArray.length + substrings.length];
+            System.arraycopy(stringArray, 0, newArray, 0, stringArray.length);
+            System.arraycopy(substrings, 0, newArray, stringArray.length, substrings.length);
+
+            // Update the reference to the new array
+            stringArray = newArray;
+
+            // Check if the user wants to stop (pressing Enter key)
+            if (!(userInput.isEmpty())) {
+                break;
+            }
+        }
+
+
+
+        for (int i = 0; i<hall.length; i++){
+            for (int j= 0; j<hall[i].length; j++){
+                char aphabet = (char) ('A' + i ) ;
+                if(aphabet == c && number == j){
+                    System.out.println("founded"+" row : "+i+ " cols : "+j);
+                    hall[i][j] = "BO";
+                }
+            }
+        }
+    }
+
 
     public static void mainMenu(){
-        System.out.println("""
+        System.out.print("""
                 [[ Application Menu ]]
                 <A> Booking
                 <B> Hall
@@ -64,8 +177,8 @@ public class Main {
                 """);
     }
 
-    public static void showTimeMenu(){
-        System.out.println("""
+     public static void showTimeMenu(){
+        System.out.print("""
                 +++++++++++++++++++++++++++++++++++++++++++++++
                 # Daily Showtime of CSTAD HALL:
                 # A) Morning (10:00AM - 12:30PM)
@@ -74,4 +187,6 @@ public class Main {
                 ++++++++++++++++++++++++++++++++++++++++++++++
                 """);
     }
+
+
 }
